fb2374736b9730f49386c788fef8992cdfcdbe98
WILMES <maxencewilmes@gmail.com>
2021-01-12 19:27:00 +0100

    cert setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const keyFile  = fs.readFileSync('../cert/key.pem');
+  const certFile = fs.readFileSync('../cert/cert.pem');
+
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile
+    }
+  });
   app.enableCors();
   await app.listen(3000);
 }
